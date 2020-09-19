9d5ab7655e0a6abcd461b4e49739f7e5d1f5a5ce
pwojcik1 <kyrtap44@gmail.com>
2020-09-19 14:06:37 +0200

    P. Wójcik zadanie 1 wymaganie 2

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -3,7 +3,13 @@ package com.example.demo;
 public class Greet {
 
     public String greet(String name) {
-        return String.format("Witaj, %s", name);
+
+        if (name != null) {
+            return String.format("Witaj, %s", name);
+
+        } else {
+            return "Witaj, mój przyjacielu";
+        }
     }
 
 }
