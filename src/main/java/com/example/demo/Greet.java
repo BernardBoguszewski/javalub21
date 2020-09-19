9cdccade7a5940afe7c17869f7ada0e87d764aa1
Jacek Jozefczuk <jjoze@go2.pl>
2020-09-19 14:12:39 +0200

    JacekJ-Zadanie1 Wymaganie 2

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class Greet {
 
     public String greet(String name) {
+        if (name==null) {return "Witaj, mój przyjacielu";} else
         return String.format("Witaj, %s", name);
     }
 
