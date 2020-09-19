c7f8c725d13de1d60f986a83442f4ff91ed68f4e
pwojcik1 <kyrtap44@gmail.com>
2020-09-19 14:16:20 +0200

    P. Wójcik zadanie 1 wymaganie 2 poprawa

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -3,13 +3,7 @@ package com.example.demo;
 public class Greet {
 
     public String greet(String name) {
-
-        if (name != null) {
-            return String.format("Witaj, %s", name);
-
-        } else {
-            return "Witaj, mój przyjacielu";
-        }
+        return (name != null) ? String.format("Witaj, %s", name) : "Witaj, mój przyjacielu";
     }
 
 }
