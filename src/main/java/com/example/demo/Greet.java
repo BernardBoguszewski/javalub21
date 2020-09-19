f41f1dd07625af658b2c80e54e4e81edd0ab6e3d
Przemek <przemyslaw.serwatko@gmail.com>
2020-09-19 14:33:28 +0200

    zad 1 wymagnie 3 poprawa

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -5,21 +5,10 @@ public class Greet {
     public String greet(String name) {
         if (name == null) {
             return "Witaj, mój przyjacielu";
-        } else if (isUpperCase(name)) {
+        } else if (name.equals(name.toUpperCase())) {
             return String.format("WITAJ %s!", name.toUpperCase());
         }
         return String.format("Witaj, %s", name);
-    }
-
-    private boolean isUpperCase(String name) {
-        char[] charArray = name.toCharArray();
-
-        for(int i=0; i < charArray.length; i++){
-            if( !Character.isUpperCase( charArray[i] ))
-                return false;
-        }
-
-        return true;
     }
 
 }
