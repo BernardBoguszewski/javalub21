c499ee8ae7b5d8ad9024891473170ff7b54a9bf7
Przemek <przemyslaw.serwatko@gmail.com>
2020-09-19 15:41:03 +0200

    refactor wymagania 6 zad1

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -13,16 +13,20 @@ public class Greet {
             if (table.length == 2) {
                 return String.format("%s i %s, witajcie!", table[0], table[1]);
             } else if (table.length == 3) {
-                if (table[0].equals(table[0].toUpperCase())) {
-                    return String.format("%s i %s, witajcie! WITAJ %s!", table[1], table[2], table[0].toUpperCase());
-                } else if (table[1].equals(table[1].toUpperCase())) {
-                    return String.format("%s i %s, witajcie! WITAJ %s!", table[0], table[2], table[1].toUpperCase());
-                } else if (table[2].equals(table[2].toUpperCase())) {
-                    return String.format("%s i %s, witajcie! WITAJ %s!", table[0], table[1], table[2].toUpperCase());
-                }
-                return String.format("%s, %s i %s, witajcie!", table[0], table[1], table[2]);
+                return greetingWithOneCapitalName(table);
             }
         }
         return String.format("Witaj, %s", name);
+    }
+
+    private String greetingWithOneCapitalName(String[] table) {
+        if (table[0].equals(table[0].toUpperCase())) {
+            return String.format("%s i %s, witajcie! WITAJ %s!", table[1], table[2], table[0].toUpperCase());
+        } else if (table[1].equals(table[1].toUpperCase())) {
+            return String.format("%s i %s, witajcie! WITAJ %s!", table[0], table[2], table[1].toUpperCase());
+        } else if (table[2].equals(table[2].toUpperCase())) {
+            return String.format("%s i %s, witajcie! WITAJ %s!", table[0], table[1], table[2].toUpperCase());
+        }
+        return String.format("%s, %s i %s, witajcie!", table[0], table[1], table[2]);
     }
 }
