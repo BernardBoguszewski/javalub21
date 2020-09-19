dce4bf3824c7d2ee6c6078c668afaacd77667bd3
Lukasz Waclawek <waclawek.it@gmail.com>
2020-09-19 15:00:16 +0200

    lukaszw-zadanie1-wymaganie5 + format

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -3,20 +3,20 @@ package com.example.demo;
 public class Greet {
 
     public String greet(String name) {
-        if(name == null){
+        if (name == null) {
             return "Witaj, mój przyjacielu";
         }
-        if(isUpperCase(name)) {
+        if (isUpperCase(name)) {
             return String.format("WITAJ %s!", name);
         }
-        if(name.contains(",")){
+        if (name.contains(",")) {
             String[] strings = name.split(",");
-            if(strings.length == 2){
+            if (strings.length == 2) {
                 return String.format("%s i %s, witajcie!", strings[0], strings[1]);
-            }if(strings.length == 3){
+            }
+            if (strings.length == 3) {
                 return String.format("%s, %s i %s, witajcie!", strings[0], strings[1], strings[2]);
             }
-
         }
         return String.format("Witaj, %s", name);
     }
@@ -25,7 +25,7 @@ public class Greet {
         return name.contains(",");
     }
 
-    private boolean isUpperCase(String name){
+    private boolean isUpperCase(String name) {
         return name.equals(name.toUpperCase());
     }
 
