e4ec70e81078d092d9c0c5f7bcd40447a7a28bcc
Przemek <przemyslaw.serwatko@gmail.com>
2020-09-20 11:13:33 +0200

    Zad 4 wymagania 1-2

--- src/main/java/com/example/demo/StringCalculator.java
+++ src/main/java/com/example/demo/StringCalculator.java
@@ -1,5 +1,17 @@
 package com.example.demo;
 
 public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers == null || numbers.isBlank()) {
+            return 0;
+        }
+        String[] numbersTable = numbers.split(",");
+        int sum = 0;
 
+        for (String s : numbersTable) {
+            sum = sum + Integer.parseInt(s);
+        }
+
+        return sum;
+    }
 }
--- src/test/java/com/example/demo/StringCalculatorTest.java
+++ src/test/java/com/example/demo/StringCalculatorTest.java
@@ -1,7 +1,43 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
+    @Test
+    void shouldReturn0WhenGivenStringIsNull() {
+        StringCalculator stringCalculator = new StringCalculator();
 
+        int result = stringCalculator.add(null);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void shouldReturn0WhenGivenStringIsEmpty() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int result = stringCalculator.add("");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void shouldAddTwoNumbersSeparatedByComma() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int result = stringCalculator.add("3,5");
+
+        assertEquals(8, result);
+    }
+
+    @Test
+    void shouldAddThreeNumbersSeparatedByComma() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int result = stringCalculator.add("2,3,5");
+
+        assertEquals(10, result);
+    }
 }
