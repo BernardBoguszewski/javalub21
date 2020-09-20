11b5834c58d3a9ced084b807afd68b664c43fda6
Przemek <przemyslaw.serwatko@gmail.com>
2020-09-20 10:15:52 +0200

    Zad 3 wymagania 3-5

--- src/main/java/com/example/demo/FizzBuzzWhizz.java
+++ src/main/java/com/example/demo/FizzBuzzWhizz.java
@@ -5,9 +5,41 @@ public class FizzBuzzWhizz {
         if (digit == null) {
             return "Nie dla null!";
         }
+        if (digit == 3) {
+            return "FizzWhizz";
+        }
+        if (digit == 5) {
+            return "BuzzWhizz";
+        }
+        if (isDigitPrime(digit)) {
+            return "Whizz";
+        }
+        if (digit % 3 == 0 && digit % 5 == 0) {
+            return "FizzBuzz";
+        }
         if (digit % 3 == 0) {
             return "Fizz";
         }
+        if (digit % 5 == 0) {
+            return "Buzz";
+        }
         return null;
+    }
+
+    private boolean isDigitPrime(Integer digit) {
+        if (digit == 2) {
+            return true;
+        }
+
+        int counter = 2;
+        for (int i = 3; i <= digit; i++) {
+            if (digit % i == 0) {
+                counter++;
+                if (counter > 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
