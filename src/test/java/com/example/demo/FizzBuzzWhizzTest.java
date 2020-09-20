b97acc2a7845f3ba8b073881bf1839274a728caf
Przemek <przemyslaw.serwatko@gmail.com>
2020-09-20 09:18:21 +0200

    Zad 3 wymaganie 2

--- src/test/java/com/example/demo/FizzBuzzWhizzTest.java
+++ src/test/java/com/example/demo/FizzBuzzWhizzTest.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,11 +11,20 @@ class FizzBuzzWhizzTest {
     @Test
     void testShouldReturnDefaultStringForNull() {
         FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
-        Integer givenDigit = null;
 
-        String result = fizzBuzzWhizz.fizzBuzzWhizz(givenDigit);
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(null);
 
         assertEquals("Nie dla null!", result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3", "6", "15", "18", "30", "102"})
+    void testShouldReturnFizzIfDigitIsDivisibleByThree(Integer digit) {
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        String result = fizzBuzzWhizz.fizzBuzzWhizz(digit);
+
+        assertEquals("Fizz", result);
     }
 
 }
