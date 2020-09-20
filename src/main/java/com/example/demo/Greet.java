ec9452c207a5c3c974561242b368ef1f47aa234a
Iza <izabelajus5@gmail.com>
2020-09-20 11:04:53 +0200

    wymaganie 2

--- src/main/java/com/example/demo/Greet.java
+++ src/main/java/com/example/demo/Greet.java
@@ -3,10 +3,14 @@ package com.example.demo;
 public class Greet {
 
     public String greet(String name) {
-        if (name != null){
+        if (name != null) {
             return String.format("Witaj, %s", name);
+
+        } else if (name != null && name.equals(name.toUpperCase())) {
+            return String.format("WITAJ, %s,", name.toUpperCase());
+
         } else {
-            return String.format("Witaj mój przyjacielu! ");
+            return String.format("Witaj, mój przyjacielu! ");
         }
     }
 }
