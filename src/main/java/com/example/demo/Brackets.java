61287eefbf7eac35bafb429c2ba788233f075d57
Jacek Jozefczuk <jjoze@go2.pl>
2020-09-20 15:07:43 +0200

    Jacek J : Zadanie 2 - modify [for loop] and last if statement.

--- src/main/java/com/example/demo/Brackets.java
+++ src/main/java/com/example/demo/Brackets.java
@@ -4,12 +4,11 @@ public class Brackets {
     public boolean closedBrackets(String brackets) {
         char[] listOfBracket = brackets.toCharArray();
         int counterForBracketsToClose = 0;
-        for (int i = 0; i < listOfBracket.length; i++) {
-            if (listOfBracket[i] == '[') counterForBracketsToClose++;
+        for (char bracket : listOfBracket) {
+            if (bracket == '[') counterForBracketsToClose++;
             else counterForBracketsToClose--;
             if (counterForBracketsToClose < 0) break;
         }
-        if (counterForBracketsToClose == 0) return true;
-        else return false;
+        return counterForBracketsToClose == 0;
     }
 }
